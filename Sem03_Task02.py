0d9e8142a5fcf0f79d1815544a1db4946d95242c
A_Zavershinskiy_Win7 <yeah-men@mail.ru>
2022-09-15 16:43:15 +0200

    add Task02 of Sem03

---
+++ Sem03_Task02.py
@@ -0,0 +1,18 @@
+# Напишите программу, которая найдёт произведение пар чисел списка.
+# Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+
+from math import ceil
+
+
+def Product_Nums(list1):
+    product_list = []
+    for i in range(ceil(len(list1)/2)):
+        product = list1[i] * list1[len(list1) - i - 1]
+        product_list.append(product)
+    return product_list
+
+
+list_nums = [int(i) for i in input(
+    'Введите список из нескольких чисел через пробел: ').split()]
+print(f'{list_nums} - получившийся список')
+print(f'{Product_Nums(list_nums)} - произведение пар чисел списка')
