06f5c735e04d4495e5a502b8d3ed126a8a76b54c
A_Zavershinskiy_Win7 <yeah-men@mail.ru>
2022-09-27 16:44:03 +0200

    add Task03 of Sem04

---
+++ Sem04_Task03.py
@@ -0,0 +1,19 @@
+# Задайте последовательность чисел. Напишите программу, которая выведет список
+# неповторяющихся элементов исходной последовательности
+
+# [1, 1, 2, 3, 4, 5, 5] -> [2, 3, 4]
+
+from itertools import count
+from random import randint
+
+
+def unique_elements(nums_list):
+    result_list = []
+    for i in nums_list:
+        if nums_list.count(i) == 1:
+            result_list.append(i)
+    return result_list
+
+
+nums_list = [1, 1, 2, 3, 4, 5, 5]
+print(unique_elements(nums_list))
