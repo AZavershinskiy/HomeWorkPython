84a543b9204567258bfe24bf6c801279a974876b
A_Zavershinskiy_Win7 <yeah-men@mail.ru>
2022-09-28 19:12:44 +0200

    add Task01 of Sem05

---
+++ Sem05_Task01.py
@@ -0,0 +1,13 @@
+# Напишите программу, удаляющую из текста все слова, например, содержащие "абв".
+
+
+def word_deleter(str1, str2):
+    end_str = ' '.join(
+        [i for i in list(initial_str.split()) if word_delete not in i])
+    return end_str
+
+
+initial_str = input('Введите предложение из нескольких слов: ')
+word_delete = input('Введите слово, которое нужно удалить из предложения: ')
+
+print(f'"{word_deleter(initial_str, word_delete)}" <- предложение без удаленного слова')
