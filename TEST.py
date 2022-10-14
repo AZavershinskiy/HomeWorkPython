4b148fddd60acc6867fb3c38fde29c8a0ae978bb
A_Zavershinskiy_Win7 <yeah-men@mail.ru>
2022-10-14 18:38:42 +0200

    add Note in Test

--- TEST.py
+++ TEST.py
@@ -2,32 +2,25 @@
 import csv
 
 
-def editor():
-    with open(db.file_path, 'r', encoding='utf-8') as file:
-        search_id = int(input('Введите ID изменяемого контакта: '))
-        table = prettytable.from_csv(file)
-        table = table.get_string(start=search_id-1, end=search_id)
-        print(table)
-    input_field = int(input("""Какое поле редактировать?
-        1 - Имя
-        2 - Фамилия
-        3 - Телефон
-        4 - Примечание
- Ответ: """))
-    new_value = input('Введите новое значение: ')
-    dialect_params = dict(delimiter=';')
-    with open(db.file_path, 'r+', newline='', encoding='utf-8') as file:
-        reader = csv.reader(file, **dialect_params)
-        lines = []
-        for current_line in reader:
-            if reader.line_num == search_id+1:
-                current_line[input_field] = new_value
-            lines.append(current_line)
-        file.seek(0)
-        csv.writer(file, **dialect_params).writerows(lines)
-        file.truncate()
-        print('Готово! Запись изменена:')
-    with open(db.file_path, mode="r", encoding='utf-8') as file:
-        table = prettytable.from_csv(file)
-        table = table.get_string(start=search_id-1, end=search_id)
-        print(table)
+def finder():
+    with open(db.file_path, mode='r', encoding='utf-8') as file:
+        csv.Sniffer().sniff(file.readline())
+        answer = input("""Вам известен ID контакта?
+            1 - ДА
+            2 - НЕТ
+        Ответ: """)
+        if answer == '1':
+            search_id = int(input('Введите ID: '))
+            table = prettytable.from_csv(file)
+            table = table.get_string(start=search_id-1, end=search_id)
+            print(table)
+        elif answer == '2':
+            search_word = input('\nПоиск: ').lower()
+            for row in file:
+                if search_word in row.lower():
+                    search_id = int(row[0])
+                    table = prettytable.from_csv(file)
+                    table = table.get_string(start=search_id-1, end=search_id)
+                    print(table)
+
+# возможно, проблема в Win7
