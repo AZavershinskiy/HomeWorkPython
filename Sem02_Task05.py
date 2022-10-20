352f2172b797ba2d85adf956cebbae22acbce793
A_Zavershinskiy_Win7 <yeah-men@mail.ru>
2022-10-20 18:02:37 +0200

    Correction of solutions for Tasks of Sem02

--- Sem02_Task05.py
+++ Sem02_Task05.py
@@ -3,8 +3,24 @@
 
 from random import randint, shuffle
 
-list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-print(f'{list} - исходный список')
-shuffle(list)
-print('---')
-print(f'{list} - перемешанный список')
+
+# list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# print(f'{list1} - исходный список')
+# shuffle(list1)
+# print(f'\n{list1} - перемешанный список')
+
+
+""" или """
+
+
+list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(f'{list1} - исходный список')
+index_list = []
+list2 = []
+while len(index_list) != len(list1):
+    index = randint(0, len(list1) - 1)
+    if not (index in index_list):
+        index_list.append(index)
+for i in index_list:
+    list2.append(list1[i])
+print(f'\n{list2} - перемешанный список')
