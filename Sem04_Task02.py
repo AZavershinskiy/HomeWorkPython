32f1c9c84aeb688adc1686f0bf614a7f212b389d
A_Zavershinskiy_Win7 <yeah-men@mail.ru>
2022-10-21 20:03:56 +0200

    Correction of solutions for Tasks of Sem04

--- Sem04_Task02.py
+++ Sem04_Task02.py
@@ -3,16 +3,16 @@
 
 
 def prime_factors(n):
-   i = 2
-   list1 = []
-   while i * i <= n:
-       while n % i == 0:
-           list1.append(i)
-           n /= i
-       i += 1
-   if n > 1:
-       list1.append(int(n))
-   return list1
+    i = 2
+    list1 = []
+    while i * i <= n:
+        while n % i == 0:
+            list1.append(i)
+            n /= i
+        i += 1
+    if n > 1:
+        list1.append(int(n))
+    return list1
 
 
 n = int(input('Введите натуральное число: '))
